5538318e3ebcf5a2f2ef5eb54444d4c4e15c31bf
Sarina Nikolić <sarina.nikolic@gmail.com>
2022-07-07 10:30:53 +0200

    Edit last changes

--- src/components/categories.js
+++ src/components/categories.js
@@ -3,15 +3,11 @@ import styled from 'styled-components';
 export default function Categories({onColorChange}) {
   return (
     <StyledCategories>
-      <FilterContainer>
-        <div>
-          <select name="color-list" id="color-list" onChange={onColorChange}>
-            <option value="">color</option>
-            <option value="navy">navy</option>
-            <option value="white">white</option>
-          </select>
-        </div>
-      </FilterContainer>
+      <select name="color-list" id="color-list" onChange={onColorChange}>
+        <option value="">color</option>
+        <option value="navy">navy</option>
+        <option value="white">white</option>
+      </select>
     </StyledCategories>
   );
 }
@@ -21,8 +17,4 @@ const StyledCategories = styled.div`
   padding: 0.5rem;
   display: flex;
   justify-content: space-evenly;
-`;
-
-const FilterContainer = styled.div`
-  margin: 0.2rem;
 `;
