1eafdd70477315d7defd51eb22a3fbb54c9d9b44
Sarina Nikolić <sarina.nikolic@gmail.com>
2022-07-18 09:15:56 +0200

    last changes

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -3,18 +3,22 @@ import styled from 'styled-components';
 export default function CategoryFilter({onFilterChange}) {
   return (
     <StyledCategories>
-      <select name="color-list" id="color-list" onChange={(event) => onFilterChange(event, 'color')}>
+      <select name="color-list" id="color-list" onChange={event => onFilterChange(event, 'color')}>
         <option value="all">color</option>
         <option value="navy">navy</option>
         <option value="white">white</option>
         <option value="lightblue">lightblue</option>
+        <option value="beige">beige</option>
+        <option value="yellow">yellow</option>
+        <option value="red">red</option>
       </select>
-      <select name="brand-list" id="brand-list" onChange={(event) => onFilterChange(event, 'brand')}>
+      <select name="brand-list" id="brand-list" onChange={event => onFilterChange(event, 'brand')}>
         <option value="all">brand</option>
         <option value="&other stories">&other stories</option>
         <option value="Tommy Hilfiger">Tommy Hilfiger</option>
         <option value="Ralph Lauren">Ralph Lauren</option>
         <option value="H&M">H&M</option>
+        <option value="Zara">Zara</option>
       </select>
     </StyledCategories>
   );
